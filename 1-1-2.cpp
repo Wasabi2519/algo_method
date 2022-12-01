d4442854219f38b4a1687f1a0c55d75f28c71bfd
たしなむわさび／Wasabi2519 <fuga.maeji@gmail.com>
2022-12-01 12:36:34 +0900

    ⅲ commit

--- 1-1-2.cpp
+++ 1-1-2.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int N;
     cin >> N;
 
-    cout << N * 2 << endl;
+    cout << N % 5 << endl;
 }
---
+++ 1-1-5.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+
+    cout << 24 - X << endl;
+}
