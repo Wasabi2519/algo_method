6d982349197ce40e3216ccb297c98fa39add8d04
たしなむわさび | Wasabi2519 <fuga.maeji@gmail.com>
2023-02-03 12:29:18 +0900

    commit

---
+++ 3-2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N; // 入力を整数型として受け取る  
+  vector<int> A(N);
+  for (int i=0; i<N; i++) {
+    cin >> A[i]; // 入力を整数型動的配列 として受け取る
+  }
+
+  int ans = 1; // 答え
+  for (int item: A) { // A の各要素を掛け合わせる  
+    ans *= item; 
+  }
+
+  cout << ans << endl;  
+  return 0;
+}
